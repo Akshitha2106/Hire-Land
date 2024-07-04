dcd0f4601032c7688697e6f894f06a283b816cd4
Akshitha2106 <akshithamittapalli@gmail.com>
2024-07-04 18:54:16 +0530

    Setting up backend and added user register,login and logout functionalities

---
+++ Backend/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleWare } from "./middlewares/error.js";
+
+const app = express();
+
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/user", userRouter);
+app.use("/api/application", applicationRouter);
+app.use("/api/job", jobRouter);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    message: "Page not found",
+  });
+});
+
+dbConnection();
+
+app.use(errorMiddleWare);
+
+export default app;
